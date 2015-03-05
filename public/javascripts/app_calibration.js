10440297818a7e3a0849d4a53aacb40c69ed90a2
gustavo gonzales <gustavo.gonda@gmail.com>
2015-03-05 16:57:28 -0500

    moejorado ejemplo  de calibration, seguidor de mirada, pupila, y rostro

--- public/javascripts/app_calibration.js
+++ public/javascripts/app_calibration.js
@@ -1,20 +1,44 @@
 
 			window.onload = function () {
+				var bolita1 = $('.bolita1')
+				var bolita2 = $('.bolita2')
 				var height = 480;
-				var width = 640;
+				var width = height*(1.3333333333);
 
 				var cGaze = new camgaze.Camgaze(
 					width, 
 					height, 
 					"mainCanvas"
 				);
+				var faceDetector = new camgaze.CVUtil.HaarDetector(
+					camgaze.cascades.frontalface,
+					width,
+					height
+				);
 				var eyeTracker = new camgaze.EyeTracker(width, height);
 				var eyeFilter = new camgaze.EyeFilter();
 				var drawer = new camgaze.drawing.ImageDrawer();
 
 				var frameOp = function (image_data, video) {
-					var trackingData = eyeTracker.track(image_data, video);
+				var trackingData = eyeTracker.track(image_data, video);
+				var faceRects = faceDetector.detectObjects(
+					video,
+					1.1,
+					1
+				);
 
+				faceRects.forEach(
+					function (face) {
+						image_data = drawer.drawRectangle(
+							image_data,
+							face,
+							face.width,
+							face.height,
+							3,
+							"red"
+						);
+					}
+				);
 					var gazeList = eyeFilter.getFilteredGaze(trackingData);
 
                     // new HAAR.Detector(haarcascade_frontalface_alt, Parallel)
@@ -31,7 +55,15 @@
 							function (eye) {
 								//console.log([eye.eyeData.getHaarRectangle().x,eye.eyeData.getHaarRectangle().y].join(":"));
 								//console.log([eye.centroid.unfiltered.x,eye.centroid.unfiltered.y].join(":"));
-								console.log(eye.gazeVector);
+								//console.log(eye.gazeVector);
+								bolita1.css({
+									top: eye.centroid.unfiltered.y+'px',
+									left: eye.centroid.unfiltered.x+'px'
+								});
+								bolita2.css({
+									top: eye.eyeData.getHaarRectangle().y+'px',
+									left: eye.eyeData.getHaarRectangle().x+'px'
+								});
         image_data = drawer.drawRectangle(
             image_data,
             eye.eyeData.getHaarRectangle(),
